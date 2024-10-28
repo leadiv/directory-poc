da1b248a3a9c33eb52426fde652019d328fe781c
leadiv <leadiv@gmail.com>
2024-10-28 19:36:26 -0400

    red: show primary member first

--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -4,7 +4,6 @@ from mock_household import single_household
 from display_household import display_household
 
 ''',
-            '<img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher" /> <span data-primary data-membership="Participant (CBC)" data-status="active">Rob Kocher</span>,',
             '<img src=""https://avatars.planningcenteronline.com/uploads/person/49166778-1551397767/avatar.2.jpg alt="Tammy Kocher" /> <span data-membership="Participant (CBC)" data-status="active">Tammy Kocher</span>,',
             '<img src="https://avatars.planningcenteronline.com/uploads/person/49657796-1551907477/avatar.3.png" alt="Charisa Kocher" /> <span data-membership="Participant (CBC)" data-status="active">Charisa Kocher</span>,',
             '<img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher" /> <span data-membership="Friend of CBC" data-status="active">Kupo Kocher</span>'
@@ -15,6 +14,7 @@ class TestHousehold(unittest.TestCase):
         expected = ''.join((
             '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/><br />',
             'The Kocher Household<br />',
+            '<img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher" /> <span data-primary data-membership="Participant (CBC)" data-status="active">Rob Kocher</span>,',
         ))
         actual = display_household(single_household)
 
