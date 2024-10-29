e694850f6c1184551abebf7fd2780f9689e5410e
leadiv <leadiv@gmail.com>
2024-10-28 23:10:58 -0400

    green: display primary member

--- scripts/display_household.py
+++ scripts/display_household.py
@@ -1,20 +1,63 @@
-def transformHousehold(household):
+def transform_person(id, people_info, is_primary):
+    person_found = [
+        info for info in people_info
+        if info.get('id', '') == id
+    ]
+
+    try:
+        return {
+            'avatar': person_found[0].get('attributes', {}).get('avatar', ''),
+            'name': person_found[0].get('attributes', {}).get('name', ''),
+            'is_primary': is_primary,
+            'membership': person_found[0].get('attributes', {}).get('membership', ''),
+            'status': person_found[0].get('attributes', {}).get('status', ''),
+        }
+
+    except:
+        return None
+
+def transform_household(household, people_info):
     return {
         'household_image': household.get('attributes', {}).get('avatar'),
-        'household_name': household.get('attributes', {}).get('name')
+        'household_name': household.get('attributes', {}).get('name'),
+        'household_members': [
+            transform_person(household.get('attributes', {}).get('primary_contact_id', ''), people_info, True)
+        ]
+
     }
 
-def createHouseholdHtml(household):
+def create_member_html(member):
+    if member.get('is_primary'):
+        data_primary = f'data-primary '
+    else:
+        data_primary = ''
+
+    data_membership = f'data-membership="{member.get('membership')}" '
+    data_status = f'data-status="{member.get('status')}"'
+
     return ''.join((
-        f'<img src="{household.get('household_image')}" alt="{household.get('household_name')}"/><br />',
-        f'The {household.get('household_name')}<br />'
+        f'<img src="{member.get('avatar')}" alt="{member.get('name')}"/>',
+        f'<span {data_primary}{data_membership}{data_status}>',
+        member.get('name'),
+        '</span>'
+    ))
+
+def create_household_html(household):
+    return ''.join((
+        f'<img src="{household.get('household_image')}" alt="{household.get('household_name')}"/><br/>',
+        f'The {household.get('household_name')}<br/>',
+        ''.join([
+            create_member_html(member) for member in household.get('household_members', [])
+            if member
+        ])
     ))
 
 def display_household(household_json):
     household_data = household_json.get('data', [])
+    household_people = household_json.get('included', [])
 
-    household_info = [transformHousehold(household)  for household in household_data]
+    household_info = [transform_household(household, household_people)  for household in household_data]
 
-    html = [createHouseholdHtml(household) for household in household_info]
+    html = [create_household_html(household) for household in household_info]
 
     return ''.join(html)
--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -11,10 +11,12 @@ from display_household import display_household
 class TestHousehold(unittest.TestCase):
 
     def test_display_household(self):
+        self.maxDiff = None
+
         expected = ''.join((
-            '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/><br />',
-            'The Kocher Household<br />',
-            '<img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher" /> <span data-primary data-membership="Participant (CBC)" data-status="active">Rob Kocher</span>,',
+            '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/><br/>',
+            'The Kocher Household<br/>',
+            '<img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span data-primary data-membership="Participant (CBC)" data-status="active">Rob Kocher</span>',
         ))
         actual = display_household(single_household)
 
