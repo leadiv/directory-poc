8ccee11f6a9e05b42b4ec2107c6c4577451f6931
leadiv <leadiv@gmail.com>
2024-10-29 01:22:12 -0400

    green: update logic for last separator

--- scripts/display_household.py
+++ scripts/display_household.py
@@ -31,7 +31,7 @@ def transform_household(household, people_info):
 
     }
 
-def create_member_html(member):
+def create_member_html(member, is_last_member):
     if member.get('is_primary'):
         data_primary = f'data-primary '
     else:
@@ -40,22 +40,27 @@ def create_member_html(member):
     data_membership = f'data-membership="{member.get('membership')}" '
     data_status = f'data-status="{member.get('status')}"'
 
+    if is_last_member:
+        separator = ''
+    else:
+        separator = ','
+
     return ''.join((
         '<li>',
         f'<img src="{member.get('avatar')}" alt="{member.get('name')}"/>',
         f'<span {data_primary}{data_membership}{data_status}>',
         member.get('name'),
-        '</span>,',
+        f'</span>{separator}',
         '</li>'
     ))
 
 def create_household_html(household):
     return ''.join((
-        f'<img src="{household.get('household_image')}" alt="{household.get('household_name')}"/><br/>',
+        f'<img src="{household.get('household_image')}" alt="{household.get('household_name')}"/>',
         f'<h2>The {household.get('household_name')}</h2>',
         '<ul>',
         ''.join([
-            create_member_html(member) for member in household.get('household_members', [])
+            create_member_html(member, index == len(household.get('household_members', [])) - 1) for index, member in enumerate(household.get('household_members', []))
             if member
         ]),
         '</ul>'
