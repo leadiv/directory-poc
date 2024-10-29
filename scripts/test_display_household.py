b686157068499e6bb75c3396b152ee63c973c178
leadiv <leadiv@gmail.com>
2024-10-29 01:21:15 -0400

    red: remove last comma and br from image
    Small changes to the HTML snippet to make it easier to style.

--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -9,13 +9,13 @@ class TestHousehold(unittest.TestCase):
         self.maxDiff = None
 
         expected = ''.join((
-            '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/><br/>',
+            '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/>',
             '<h2>The Kocher Household</h2>',
             '<ul>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span data-primary data-membership="Participant (CBC)" data-status="active">Rob Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49166778-1551397767/avatar.2.jpg" alt="Tammy Kocher"/><span data-membership="Participant (CBC)" data-status="active">Tammy Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49657796-1551907477/avatar.3.png" alt="Charisa Kocher"/><span data-membership="Participant (CBC)" data-status="active">Charisa Kocher</span>,</li>',
-            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher"/><span data-membership="Friend of CBC" data-status="active">Kupo Kocher</span>,</li>'
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher"/><span data-membership="Friend of CBC" data-status="active">Kupo Kocher</span></li>'
             '</ul>'
         ))
         actual = display_household(single_household)
