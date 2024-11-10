4d83d357d366ca1fdf039f78ce0427381876c6c6
leadiv <leadiv@gmail.com>
2024-11-10 14:56:58 -0500

    red: should be able to display multiple members

--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -5,7 +5,7 @@ from display_household import display_household
 
 class TestHousehold(unittest.TestCase):
 
-    def test_display_household(self):
+    def test_single_display_household(self):
         self.maxDiff = None
 
         expected = ''.join((
@@ -17,7 +17,24 @@ class TestHousehold(unittest.TestCase):
         ))
         actual = display_household(single_household, ['36729400'])
 
-        self.assertEqual(expected, actual, 'it should be able to display the family household as HTML')
+        self.assertEqual(expected, actual, 'it should be able to display one family member as HTML')
+
+    def test_multiple_display_household(self):
+        self.maxDiff = None
+
+        expected = ''.join((
+            '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/>',
+            '<h2>The Kocher Household</h2>',
+            '<ul>',
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span>Rob Kocher</span>,</li>',
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49166778-1551397767/avatar.2.jpg" alt="Tammy Kocher"/><span>Tammy Kocher</span>,</li>',
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49657796-1551907477/avatar.3.png" alt="Charisa Kocher"/><span>Charisa Kocher</span>,</li>',
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher"/><span>Kupo Kocher</span></li>',
+            '</ul>'
+        ))
+        actual = display_household(single_household, ['36729400', '49166778', '49657796', '76460632'])
+
+        self.assertEqual(expected, actual, 'it should be able to display multiple amily members as HTML')
 
 if __name__ == '__main__':
     unittest.main()
