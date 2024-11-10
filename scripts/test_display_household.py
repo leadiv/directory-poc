6c75cb38cea92eb55b93934226abe23bf45d9b36
leadiv <leadiv@gmail.com>
2024-11-10 14:17:21 -0500

    red: remove the primary data

--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -12,7 +12,7 @@ class TestHousehold(unittest.TestCase):
             '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/>',
             '<h2>The Kocher Household</h2>',
             '<ul>',
-            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span data-primary>Rob Kocher</span>,</li>',
+            '<li><img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span>Rob Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49166778-1551397767/avatar.2.jpg" alt="Tammy Kocher"/><span>Tammy Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49657796-1551907477/avatar.3.png" alt="Charisa Kocher"/><span>Charisa Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher"/><span>Kupo Kocher</span></li>'
