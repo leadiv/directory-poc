4edf983ec4c63f0b17c9be56edc0336e793899b6
leadiv <leadiv@gmail.com>
2024-11-14 22:55:05 -0500

    red: use article

--- scripts/test_display_household.py
+++ scripts/test_display_household.py
@@ -15,11 +15,13 @@ class TestHousehold(unittest.TestCase):
         self.maxDiff = None
 
         expected = ''.join((
+            '<article class="directory-houshold">',
             '<h2>The Kocher Household</h2>',
             '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/>',
             '<ul>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/36729400-1524683747/avatar.2.jpeg" alt="Rob Kocher"/><span>Rob Kocher</span></li>',
-            '</ul>'
+            '</ul>',
+            '</article>',
         ))
         actual = display_household(single_household, ['36729400'])
 
@@ -29,6 +31,7 @@ class TestHousehold(unittest.TestCase):
         self.maxDiff = None
 
         expected = ''.join((
+            '<article class="directory-houshold">',
             '<h2>The Kocher Household</h2>',
             '<img src="https://avatars.planningcenteronline.com/uploads/household/6759685-1547489362/avatar.1.jpg" alt="Kocher Household"/>',
             '<ul>',
@@ -36,7 +39,8 @@ class TestHousehold(unittest.TestCase):
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49166778-1551397767/avatar.2.jpg" alt="Tammy Kocher"/><span>Tammy Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/49657796-1551907477/avatar.3.png" alt="Charisa Kocher"/><span>Charisa Kocher</span>,</li>',
             '<li><img src="https://avatars.planningcenteronline.com/uploads/person/76460632-1589316453/avatar.1.png" alt="Kupo Kocher"/><span>Kupo Kocher</span></li>',
-            '</ul>'
+            '</ul>',
+            '</article>',
         ))
         actual = display_household(single_household, ['36729400', '49166778', '49657796', '76460632'])
 
